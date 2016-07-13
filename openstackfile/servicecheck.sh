dcc97303238eb20538f9ced85458d09de5fe5439
LinZhao <linzhaolover@gmail.com>
2016-07-13 04:36:21 -0400

    fix monitor bug for no log file

--- openstackfile/servicecheck.sh
+++ openstackfile/servicecheck.sh
@@ -4,17 +4,20 @@ if [ -f /root/openrc ]; then
 . /root/openrc
 fi
 
+logfile=/home/linzhbj/servicemonitor/curlog/servicecheck.log
+logfiletmp=/home/linzhbj/servicemonitor/curlog/servicecheck.log.tmp
+
 HOSTNAME=`hostname`
 
 localtime=$(date +"%Y%m%d_%H%M%S")
 echo "=============== start ${localtime} ==========="
 
-nova service-list >> curlog/servicecheck.log
+nova service-list >  ${logfiletmp}
 if [ $? != 0 ];then
 	echo "service-list error"
 fi
 
-cat curlog/servicecheck.log | grep  "${HOSTNAME}" | grep 'down'
+cat ${logfiletmp} | grep  "${HOSTNAME}" | grep 'down'
 if [ $? != 0 ];then
 	echo "${HOSTNAME} is ok"
 else
