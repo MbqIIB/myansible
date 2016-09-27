93d753ddb2164f003987f881c0602d8733ea630a
LinZhao <linzhaolover@gmail.com>
2016-09-27 03:24:14 -0400

    add openstackfile/mitaka_servicerestart.sh

---
+++ openstackfile/mitaka_servicerestart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ $# != 1 ]
+then
+echo "usage : service_restart.sh  status|restart|stop"
+exit 1
+fi
+
+action=$1
+
+echo "action = ${action}"
+
+
+run()
+{
+service   nova-compute                       ${action}
+service   neutron-openvswitch-agent   ${action}
+}
+
+run ${action}
+
+
