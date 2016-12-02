f1e69ecfc8ac22c12b2f1b72b2755345455e5f98
LinZhao <linzhaolover@gmail.com>
2016-12-01 23:01:06 -0500

    add n1_setup_vxlan.sh n2_setup_vxlan.sh n3_setup_vxlan.sh

---
+++ newYork/devops/rdo_preinstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+
+perl -pi -e 's/SELINUX=enforcing/SELINUX= permissive/g' /etc/selinux/config
+echo 'ulimit -n 8192' >> /etc/profile
+echo "LANG=en_US.utf-8" >> /etc/environment
+echo "LC_ALL=en_US.utf-8" >> /etc/environment
+
+systemctl disable firewalld
+systemctl stop firewalld
+systemctl disable NetworkManager
+systemctl stop NetworkManager
+systemctl enable network
+systemctl start network
+
+#rpm -ivh https://repos.fedorapeople.org/repos/openstack/openstack-mitaka/rdo-release-mitaka-6.noarch.rpm
+yum install -y https://rdoproject.org/repos/rdo-release.rpm
+yum install -y centos-release-openstack-mitaka
+yum update -y
+yum install -y openstack-packstack
+
+# now reboot the machine
+#packstack --gen-answer-file=my_answers.txt
+#packstack --answer-file my_answers.txt
+
