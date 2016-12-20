119c6ea3ed382d4b2eeef29f70d1d9e56ddc39f2
LinZhao <linzhaolover@gmail.com>
2016-12-20 04:03:53 -0500

    update addflavor.sh

--- addflavor.sh
+++ addflavor.sh
@@ -4,3 +4,4 @@ nova flavor-create kvm.4c8g20G  auto 8192 20 4
 nova flavor-create docker.4c8g100G  auto 8192 100 4
 nova flavor-create docker.small  auto 2048 20 1
 nova flavor-create kvm.small auto 2048 20 1
+nova flavor-create kvm.8c16g200G  auto 16384 200 8
