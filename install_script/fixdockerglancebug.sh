100722cab38e1f0aa721ce914d725a4373e6c30b
LinZhao <linzhaolover@gmail.com>
2017-01-12 21:57:14 -0500

    add ubuntu14.04docker2kvm.sh

--- install_script/fixdockerglancebug.sh
+++ install_script/fixdockerglancebug.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#sed -i '74 a\ \ \ \ \ \ \ \ \ \ \ \ key = key.lower()' /usr/lib/python2.7/dist-packages/glanceclient/v1/images.py
+sed -i '74 a\ \ \ \ \ \ \ \ \ \ \ \ key = key.lower()' /usr/lib/python2.7/dist-packages/glanceclient/v1/images.py
+sed -i "s/^    VERSION = '1.2'/    VERSION = '1.1'/g"   /usr/lib/python2.7/dist-packages/nova/objects/pci_device.py
 source /root/openrc
 glance image-show a12e4bbd-e8a1-4c90-9e06-cfd16f46706c | grep name
