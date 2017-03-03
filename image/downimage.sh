b1a7d94a8c5b99f053ee87f305333afb0eab5ff1
LinZhao <linzhaolover@gmail.com>
2017-03-03 10:44:10 +0800

    update downimage.sh

--- image/downimage.sh
+++ image/downimage.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
 set -x
-#imagelist=( \
-#centos_7.2_docker_aufs_x86_64_v1.0.3        \
-#rhel_7.1_docker_aufs_x86_64_v0.0.4          \
-#ubuntu_14.04.2_docker_aufs_x86_64_v0.0.4    \
-#        )
 
-#imagelist=( \
-#centos_7.2_docker_aufs_ppc64le_v1.0.2                \
-#centos_7.2_docker_aufs_ppc64le_xlc13.1.3.1_v1.0.1     \
-#centos_7.2_docker_aufs_x86_64_v1.0.3                  \
-#)
+Time=$(date '+%Y%m%d_%H%M%S')
+
+Gimagelist=gimagelist.${Time}.log
+Nimagelist=nimagelist.${Time}.log
+
+glance image-list > $Gimagelist
+nova image-list > $Nimagelist
 
 imagelist=( \
-Ubuntu14.04.3-ppc64le-KVM-Docker1.9.1-v1 \
+CentOS7.3-x86_64-20G-nocheck_v1 \
 )
+
 
 for name in ${imagelist[@]}
 do
     echo ${name}
-    #suf=$(glance image-list | grep ${name} |awk -F ' ' '{print $6}')
-    suf=$(glance image-show ${name} | grep disk_format | awk -F ' ' '{print $4}')
+
+    imageid=$(grep $name $Gimagelist | awk -F '|' '{print $2}')
+    suf=$(glance image-show ${imageid} | grep "^| disk_format" | awk -F ' ' '{print $4}')
     echo ${name}.${suf}
 
     echo "start download ${name}"
     glance image-download \
-        --file ${name}.${suf} ${name}
+        --file ${name}.${suf} ${imageid}
     
-    chown opadmin:opadmin ${name}.${suf}
-
-    echo "sleep 60 s"
-    sleep 60
+#    chown opadmin:opadmin ${name}.${suf}
+    echo "sleep 10 s"
+    sleep 10
 done
 
 echo "Finish"
