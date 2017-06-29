7c11ee615dd1c0712eecd36740c9c01dee364196
LinZhao <linzhaolover@gmail.com>
2017-06-29 03:42:04 -0400

    add ctl-ossize.yml

--- addport.sh
+++ addport.sh
@@ -3,6 +3,7 @@
 set -x
 nova secgroup-list-rules default
 
+nova secgroup-add-rule default tcp 4040 4040 0.0.0.0/0
 nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
 nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
 nova secgroup-add-rule default tcp 4200 4200 0.0.0.0/0
